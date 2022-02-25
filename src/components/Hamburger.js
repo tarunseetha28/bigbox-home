4a9c1ca89fdfc503d687576c2bbd51ac3d5a7e47
tarunseetha28 <46785937+tarunseetha28@users.noreply.github.com>
2022-02-25 23:02:54 +0530

    new hamburger menu

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -5,6 +5,7 @@ import { Button, Drawer } from 'antd'
 import './Hamburger.css';
 import appstore from '../assets/astore.png';
 import gplay from '../assets/gplay.png';
+import bigboxlogo from '../assets/blacktextlogo.svg';
 
 const Hamburger = () => {
     const [visible, setVisible] = useState(false);
@@ -26,13 +27,18 @@ const Hamburger = () => {
                 icon={<img src={hamburgermenu} style={{ width: "30px" }} />}
             >
             </Button>
-            <Drawer title="menu" placement="right" onClose={onClose} visible={visible}>
-                <a href=""><p>shop</p></a>
-                <a href=""><p>what is bigbox?</p></a>
-                <a href=""><p>bigbox app</p></a>
-                <a href=""><p>for business</p></a>
-                <a href=""><p>help</p></a>
-                <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <Drawer
+                className='drawer-hamburger'
+                title={<img src={bigboxlogo} style={{ height: "30px", marginTop: "10px" }} />}
+                placement="right"
+                onClose={onClose}
+                visible={visible}>
+                <a href=""><p className='hammenu-items'>shop</p></a>
+                <a href=""><p className='hammenu-items'>what is bigbox?</p></a>
+                <a href=""><p className='hammenu-items'>bigbox app</p></a>
+                <a href=""><p className='hammenu-items'>for business</p></a>
+                <a href=""><p className='hammenu-items'>help</p></a>
+                <div style={{ display: "flex", justifyContent: "left", flexWrap: "wrap", gap: "5px" }}>
                     <a href="https://apps.apple.com/us/app/bigbox-videoshopping/id1542908804">
                         <img src={appstore} style={{ height: "40px" }} />
                     </a>
